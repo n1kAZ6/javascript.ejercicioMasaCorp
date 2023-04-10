7ed7f06dd5d5587ae65b1522b943fbd36d01b32b
niko6 <nikoalvarezzapata@gmail.com>
2023-04-10 11:57:26 +0200

    controlar valor correcto campos

--- javascript.ejercicioPesoCorporal/src/main/webapp/script/pesoCorp.js
+++ javascript.ejercicioPesoCorporal/src/main/webapp/script/pesoCorp.js
@@ -7,6 +7,17 @@ const calcular = (form) =>{
 		const masaCorp = eval(50+((form.altura.value-150)/4)*3 + (form.edad.value - 20)*0.9);
 		form.resultado.value=masaCorp;
 	}
-	else
-		alert("No ha introducido el género (hombre o mujer)");	
+	if(isNaN(form.edad.value) && isNaN(form.altura.value)){
+		alert("La edad y la altura no es un número");
+		form.resultado.value="";
+	}		
+	else if(isNaN(form.edad.value)) {
+		alert("La edad no es un número");
+		form.resultado.value="";
+	}
+	else if(isNaN(form.altura.value)) {
+		alert("La altura no es un número");
+		form.resultado.value="";
+	}
+	
 };
